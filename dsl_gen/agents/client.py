b57b88e8bdb292b2fa216ad9c557cf52e55dcaa0
Arthur Buis <74619593+ArthurBUIS@users.noreply.github.com>
2025-02-22 21:18:47 +0100

    Mistral test (#4)
    * Beginning of the Mistral AI models implementation
    
    * Add of MistralAI configurations
    
    * Update client.py
    
    ---------
    
    Co-authored-by: yimingchen <83628655+SaturnTsen@users.noreply.github.com>

--- dsl_gen/agents/client.py
+++ dsl_gen/agents/client.py
@@ -3,6 +3,7 @@
 
 from ..config import API
 from langchain_openai import ChatOpenAI
+from langchain_mistralai import ChatMistralAI
 
 import logging
 
@@ -15,13 +16,18 @@ def getClient(backend, temperature) -> ChatOpenAI:
         'openai': API.MODEL_CFG.OpenAI,
         'ollama': API.MODEL_CFG.Ollama,
         'deepseek': API.MODEL_CFG.DeepSeek,
+        'mistralai': API.MODEL_CFG.MistralAI
     }[backend]
 
     logger.info("Model loaded: %s, base_url: %s", cfg.model, cfg.base_url)
 
-    return ChatOpenAI(
-        model=cfg.model,
-        openai_api_base=cfg.base_url,
-        openai_api_key=cfg.api_key,
-        temperature=temperature
-    )
+    kwargs = {
+        'model': cfg.model,
+        'openai_api_base': cfg.base_url,
+        'openai_api_key': cfg.api_key,
+        'temperature': temperature
+    }
+
+    return (ChatMistralAI(**kwargs)
+            if backend == 'mistralai'
+            else ChatOpenAI(**kwargs))
